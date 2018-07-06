68b4f0e824cd5d3d5c6d67b5718f4e0ab6bef9ab
Alex20220 <lavbox2006@rambler.ru>
2018-07-06 16:46:47 +0300

    JUnit 1
    lection

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b5/108d68d7208100181854c45094c0eb45
@@ -0,0 +1,14 @@
+package _JUnit;
+
+public class Calc {
+	public  int add(int a, int b) {
+		return a+b;
+	}
+	public double div(int a, int b) {
+		return a/b;
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
--- Test_1/src/_JUnit/Calc.java
+++ Test_1/src/_JUnit/Calc.java
@@ -4,8 +4,11 @@ public class Calc {
 	public  int add(int a, int b) {
 		return a+b;
 	}
-	public double div(int a, int b) {
-		return a/b;
+	public int div(int d, int e) {
+		if (e==0) {
+			throw new IllegalArgumentException();
+		}
+		return d/e;
 	}
 	public static void main(String[] args) {
 
